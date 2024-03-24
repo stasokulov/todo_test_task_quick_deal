831aa235a4353466c82b95a564784a07d7b1c72e
stasokulov <stasokulov@gmail.com>
2024-03-24 14:03:45 +0300

    Поправил ссылки в роутере

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TodoListView from '../views/TodoListView.vue'
 
+const baseURL = import.meta.env.PROD ? import.meta.env.VITE_APP_BASE_URL : ''
+const homeLink = `${baseURL}/`
+const todoListLink = `${baseURL}/todo-list`
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      // path: '/',
+      path: homeLink,
       name: 'home',
       component: HomeView
     },
     {
-      path: '/todo-list',
+      // path: '/todo-list',
+      path: todoListLink,
       name: 'todo-list',
       component: TodoListView
     }
